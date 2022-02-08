061b499961c028ddfbf669f61163d0ccd98360a9
James Kainer <47008209+JamesKainer@users.noreply.github.com>
2022-02-08 11:32:09 -0800

    Adding Logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,15 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_static_path = ['_static']
+html_logo = "logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
+
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
